f139aae7580577c6e3bc66d60758ca94818d650e
fpkm9999 <fpkm99999@gmail.com>
2024-04-06 21:51:48 +0900

    2024-04-06
    2024-04-06

---
+++ src/02/13.kt
@@ -0,0 +1,15 @@
+package `02`
+
+fun main() {
+
+
+    for (i in 1..4) {
+        for (j in 1..4) {
+            // 1 2 3 4
+            if (j <= i) {
+                print("*")
+            }
+        }
+        println()
+    }
+}
